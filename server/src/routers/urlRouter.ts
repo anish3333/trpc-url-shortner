1c0085a8b7ddf0103eddcdabc021fccdc0fbdcc6
Anish Awasthi <anishawasthi16@gmail.com>
2025-01-05 03:34:38 +0530

    error handling and schema improvement in server

--- server/src/routers/urlRouter.ts
+++ server/src/routers/urlRouter.ts
@@ -1,6 +1,7 @@
 import { trpc } from "../context";
 import { z } from "zod";
 import {PrismaClient} from "@prisma/client";
+import { nanoid } from "nanoid";
 
 // routers -> collection of procedures
 // procedures -> queries, mutations, subscriptions(ws)
@@ -11,11 +12,12 @@ export const urlRouter = trpc.router({
   getOriginalUrl: trpc.procedure
                   .input(z.string())
                   .query(async ({ input }) => {
-                    const url = await prisma.url.findFirst({
+                    const url = await prisma.url.findUnique({
                       where: {
                         shortUrl: input
                       }
                     })
+                    
                     if (!url) {
                       throw new Error("Url not found");
                     }
@@ -23,10 +25,36 @@ export const urlRouter = trpc.router({
                     return url.originalUrl;
 
                   }),
+
   createShortUrl: trpc.procedure
                   .input(z.object({ url : z.string().url() }))
                   .mutation(async ({ input }) => {
-                    const shortUrl = Math.random().toString(36).substring(2, 8);
+                    
+                    // handle the case where the original url has already been shortened
+                    const url = await prisma.url.findUnique({
+                      where: {
+                        originalUrl: input.url
+                      }
+                    })
+                  
+                    if (url) {
+                      return url.shortUrl;
+                    }
+                  
+                    let isUnique = false;
+                    let shortUrl = "";
+                    // handle the case where the new shorturl is already used (collision)
+                    while (!isUnique) {
+                      shortUrl = nanoid(6);
+
+                      const exists = await prisma.url.findUnique({
+                        where: { shortUrl }
+                      });
+                      if (!exists) {
+                        isUnique = true;
+                      }
+                    }
+
                     await prisma.url.create({
                       data: {
                         originalUrl: input.url,
